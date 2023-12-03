601067437fd1e676b72fe27e31303b19dd5378e0
liux2 <xingbangliu48@gmail.com>
2023-12-03 20:28:03 +0800

    added gdrive downloader

---
+++ scripts/downloader.sh
@@ -0,0 +1,4 @@
+id="abc"                                                                                                                                                                  
+filename="filename.zip"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
